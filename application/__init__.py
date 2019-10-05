eb368304e5ecf1d3e58214d5af0149305fa12774
jonki324 <jonki324@gmail.com>
2019-10-06 01:55:52 +0900

    ロガー設定

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,11 @@
+import logging
+import logging.handlers
 import os
 from flask import Flask, render_template
 
 
 def create_app():
+    # アプリケーションの設定
     app = Flask(__name__, instance_relative_config=True)
 
     configs = {
@@ -15,8 +18,19 @@ def create_app():
     app.config.from_pyfile('application.cfg', silent=True)
     os.makedirs(app.instance_path, exist_ok=True)
 
+    # ロガー設定
+    log_dir = os.path.join(os.path.dirname(app.root_path), 'log')
+    os.makedirs(log_dir, exist_ok=True)
+    log_file = os.path.join(log_dir, app.config['LOG_FILE_NAME'])
+    handler = logging.handlers.RotatingFileHandler(log_file, "a+",
+                                                   maxBytes=3000,
+                                                   backupCount=5)
+    handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+    app.logger.addHandler(handler)
+
     @app.route('/')
     def index():
+        app.logger.info('index処理開始')
         return render_template('index.html')
 
     return app
