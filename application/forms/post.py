7b41d5411d150fa4f745a759b3e0ccede0c1f895
jonki324 <jonki324@gmail.com>
2019-10-12 18:42:37 +0900

    記事更新処理追加

--- application/forms/post.py
+++ application/forms/post.py
@@ -1,7 +1,7 @@
 from flask_uploads import UploadSet, IMAGES
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
-from wtforms import FileField, StringField, TextAreaField, HiddenField
+from wtforms import FileField, StringField, TextAreaField, HiddenField, ValidationError
 from wtforms.validators import DataRequired, Length
 
 images = UploadSet('images', IMAGES)
@@ -15,6 +15,10 @@ class PostForm(FlaskForm):
                          validators=[DataRequired('必須です'),
                                      Length(max=200, message='200桁までです')])
     image = FileField('ピクチャ',
-                      validators=[DataRequired('必須です'),
-                                  FileAllowed(['jpg'], message='jpgファイルのみです')])
+                      validators=[FileAllowed(['jpg'], message='jpgファイルのみです')])
     is_img_saved = HiddenField()
+
+    def validate_image(self, field):
+        if not field.data:
+            if not self.is_img_saved.data:
+                raise ValidationError('必須です')
--- application/views/post.py
+++ application/views/post.py
@@ -1,4 +1,3 @@
-import base64
 from application.forms.post import PostForm
 from application.models.user import User, db
 from application.models.blog_post import BlogPost
@@ -29,6 +28,36 @@ def add():
         db.session.commit()
 
         flash('投稿しました。', 'success')
+        return redirect(url_for('blog_view.blog', user_id=current_user.user_id))
+
+    return render_template('post.html', form=form)
+
+
+@post_view.route('/post/upd/<post_id>', methods=['GET', 'POST'])
+@login_required
+def upd(post_id):
+    current_app.logger.info('記事更新処理開始')
+
+    try:
+        user, post = db.session.query(User, BlogPost).filter(db.and_(User.id == current_user.id,
+                                                                     BlogPost.id == post_id)).first()
+    except Exception:
+        flash('記事がありません。', 'danger')
+        return redirect(url_for('blog_view.blog', user_id=current_user.user_id))
+
+    form = PostForm(obj=post, is_img_saved='y')
+
+    if request.method == 'POST' and form.validate_on_submit():
+        current_app.logger.info('記事更新処理開始')
+        post.title = form.title.data
+        post.body = form.body.data
+        if form.image.data:
+            post.image = form.image.data.read()
+
+        db.session.add(post)
+        db.session.commit()
+
+        flash('更新しました。', 'success')
         return redirect(url_for('blog_view.blog', user_id=current_user.user_id))
 
     return render_template('post.html', form=form)
