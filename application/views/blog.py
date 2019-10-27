54f43db58012e59d4918f3e0cad815aa90e818b6
jonki324 <jonki324@gmail.com>
2019-10-27 16:37:23 +0900

    ブログ画面リファクタリング

--- application/views/blog.py
+++ application/views/blog.py
@@ -1,12 +1,14 @@
 from application.forms.blog import BlogForm
 from application.models.user import User, db, bookmark_users
 from application.models.blog_post import BlogPost
-from flask import (Blueprint, current_app, flash,
-                   redirect, render_template, request, url_for)
+from flask import (Blueprint, current_app,
+                   render_template, request)
 from flask_paginate import Pagination, get_page_parameter
 from flask_login import current_user
 
 blog_view = Blueprint('blog_view', __name__)
+
+PER_PAGE = 9
 
 
 @blog_view.route('/blog/<user_id>')
@@ -16,49 +18,88 @@ def blog(user_id):
     bookmark_search = request.args.get('bookmark', default=False)
     keyword = request.args.get('keyword', default='')
 
-    form = BlogForm(keyword=keyword)
+    form = BlogForm(request.args)
 
-    user = db.session.query(User).filter(User.user_id == user_id).first()
+    user = fetch_blog_user_by_blog_user_id(user_id)
 
     profile = user.profile
 
-    bmk_users = user.bookmark_users
+    bookmark_users_by_blog_user = user.bookmark_users
 
     if bookmark_search:
+        current_app.logger.info('ブログユーザーのブックマーク記事取得')
         posts = []
         bmk_posts = user.bookmark_posts
         for post in bmk_posts:
             posts.append(post.bookmark_posts)
     elif keyword != '':
-        posts = db.session.query(BlogPost).filter(BlogPost.author_id == user.id,
-                                                  db.or_(BlogPost.title.like('%{}%'.format(keyword)),
-                                                         BlogPost.body.like('%{}%'.format(keyword)))).\
-            order_by(BlogPost.created_at.desc()).all()
+        posts = []
+        if form.validate():
+            current_app.logger.info('キーワード検索処理開始: {}'.format(keyword))
+            posts = fetch_blog_users_post_by_keyword(user.id, keyword)
     else:
+        current_app.logger.info('ブログユーザーの記事全件取得')
         posts = user.posts
 
-    page = request.args.get(get_page_parameter(), type=int, default=1)
-    res = posts[(page - 1) * 9: page * 9]
-    pagination = Pagination(page=page, total=len(posts), per_page=9, css_framework='bootstrap4', alignment='center')
+    posts, pagination = get_pagination(posts)
 
-    bookmarks = []
+    current_user_bookmarks = []
     if current_user.is_authenticated:
-        cur_user = db.session.query(User).filter(User.id == current_user.id).first()
-        bookmarks = cur_user.bookmark_posts
+        current_app.logger.info('ログインユーザーのブックマーク記事取得処理開始')
+        current_user_bookmarks = fetch_bookmark_posts_from_current_user()
 
-    bookmark_info = {}
-    stmt = db.select([db.func.count(bookmark_users.c.bookmark_user_id).label('cnt')]).where(
-        bookmark_users.c.bookmark_user_id == user.id)
-    result = db.engine.execute(stmt)
-    bookmark_count = result.fetchone()['cnt']
-    bookmark_info['bookmark_count'] = bookmark_count
+    profile_bookmark_info = {'bookmark_count': fetch_bookmark_user_count_target_blog_user(user.id),
+                             'is_bookmarked': False}
 
     if current_user.is_authenticated and current_user.user_id != user_id:
-        stmt = db.select([db.func.count(bookmark_users.c.bookmark_user_id).label('cnt')]).where(
-            db.and_(bookmark_users.c.bookmark_user_id == user.id, bookmark_users.c.user_id == current_user.id))
-        result = db.engine.execute(stmt)
-        is_bookmark = result.fetchone()['cnt']
+        current_app.logger.info('ログインユーザーがブログユーザーをブックマークしているか')
+        profile_bookmark_info['is_bookmarked'] = is_bookmarked_to_blog_user_from_cur_user(user.id)
 
-        bookmark_info['is_bookmark'] = bool(is_bookmark)
+    return render_template('blog.html', form=form, user=user, profile=profile,
+                           bookmark_users_by_blog_user=bookmark_users_by_blog_user,
+                           posts=posts, pagination=pagination,
+                           current_user_bookmarks=current_user_bookmarks,
+                           profile_bookmark_info=profile_bookmark_info)
 
-    return render_template('blog.html', form=form, user=user, bmk_users=bmk_users, bookmarks=bookmarks, bookmark_info=bookmark_info, profile=profile, posts=res, pagination=pagination)
+
+def fetch_blog_user_by_blog_user_id(blog_user_id):
+    query = db.session.query(User)
+    query = query.filter(User.user_id == blog_user_id)
+    return query.first()
+
+
+def fetch_blog_users_post_by_keyword(blog_user_id, keyword):
+    query = db.session.query(BlogPost)
+    query = query.filter(BlogPost.author_id == blog_user_id,
+                         db.or_(BlogPost.title.like('%{}%'.format(keyword)),
+                                BlogPost.body.like('%{}%'.format(keyword))))
+    query = query.order_by(BlogPost.created_at.desc())
+    return query.all()
+
+
+def get_pagination(posts):
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    pagination = Pagination(page=page, total=len(posts), per_page=PER_PAGE,
+                            css_framework='bootstrap4', alignment='center')
+    res = posts[(page - 1) * PER_PAGE: page * PER_PAGE]
+    return res, pagination
+
+
+def fetch_bookmark_posts_from_current_user():
+    cur_user = db.session.query(User).filter(User.id == current_user.id).first()
+    return cur_user.bookmark_posts
+
+
+def fetch_bookmark_user_count_target_blog_user(blog_user_id):
+    stmt = db.select([db.func.count(bookmark_users.c.bookmark_user_id).label('cnt')])
+    stmt = stmt.where(bookmark_users.c.bookmark_user_id == blog_user_id)
+    result = db.engine.execute(stmt)
+    return result.fetchone()['cnt']
+
+
+def is_bookmarked_to_blog_user_from_cur_user(blog_user_id):
+    stmt = db.select([db.func.count(bookmark_users.c.bookmark_user_id).label('cnt')])
+    stmt = stmt.where(db.and_(bookmark_users.c.bookmark_user_id == blog_user_id,
+                              bookmark_users.c.user_id == current_user.id))
+    result = db.engine.execute(stmt)
+    return bool(result.fetchone()['cnt'])
---
+++ tests/test_blog.py
@@ -0,0 +1,166 @@
+import os
+import time
+
+
+def signup(client):
+    data1 = {'user_id': 'user01', 'email': 'user01@email1.com',
+            'password': 'pass', 'confirm': 'pass'}
+    data2 = {'user_id': 'user02', 'email': 'user02@email2.com',
+            'password': 'pass', 'confirm': 'pass'}
+    client.post('/signup', data=data1, follow_redirects=True)
+    time.sleep(0.2)
+    client.post('/signup', data=data2, follow_redirects=True)
+
+
+def login(client):
+    user = {'email': 'user01@email1.com', 'password': 'pass'}
+    client.post('/login', data=user, follow_redirects=True)
+
+
+def login_user02(client):
+    user = {'email': 'user02@email2.com', 'password': 'pass'}
+    client.post('/login', data=user, follow_redirects=True)
+
+
+def logout(client):
+    client.get('/logout', follow_redirects=True)
+
+
+def add_posts_user01(client):
+    login(client)
+    for i in range(10):
+        title = 'u01_title{}'.format(i)
+        body = 'u01_body{}'.format(i)
+        image = 'test03.jpg'
+        file_path = os.path.join('./tests/assets', image)
+        is_img_saved = ''
+        with open(file_path, 'rb') as f:
+            data = {
+                'title': title,
+                'body': body,
+                'image': (f, f.name),
+                'is_img_saved': is_img_saved
+            }
+            client.post('/post/add', data=data, follow_redirects=True,
+                        content_type='multipart/form-data')
+        time.sleep(1)
+    logout(client)
+
+
+def add_posts_user02(client):
+    login_user02(client)
+    for i in range(3):
+        title = 'u02_title{}'.format(i)
+        body = 'u02_body{}'.format(i)
+        image = 'test04.jpg'
+        file_path = os.path.join('./tests/assets', image)
+        is_img_saved = ''
+        with open(file_path, 'rb') as f:
+            data = {
+                'title': title,
+                'body': body,
+                'image': (f, f.name),
+                'is_img_saved': is_img_saved
+            }
+            client.post('/post/add', data=data, follow_redirects=True,
+                        content_type='multipart/form-data')
+        time.sleep(0.5)
+    logout(client)
+
+
+def test_setup(client):
+    signup(client)
+    add_posts_user01(client)
+    add_posts_user02(client)
+
+
+def test_show_blog_not_login(client):
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'プロフィール設定' not in rv.data.decode('utf-8')
+
+
+def test_show_blog_login(client):
+    login(client)
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'プロフィール設定' in rv.data.decode('utf-8')
+
+
+def test_invalid_search_post(client):
+    query = {
+        'keyword': '0123456789' * 20 + 'x'
+    }
+    rv = client.get('/', query_string=query, follow_redirects=True)
+    assert '200桁までです' in rv.data.decode('utf-8')
+
+
+def test_valid_search_post(client):
+    query = {
+        'keyword': '0123456789'*20
+    }
+    rv = client.get('/', query_string=query, follow_redirects=True)
+    assert '200桁までです' not in rv.data.decode('utf-8')
+
+
+def test_search_post_default(client):
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'u01_title9' in rv.data.decode('utf-8')
+    assert 'u01_title0' not in rv.data.decode('utf-8')
+    assert 'u02' not in rv.data.decode('utf-8')
+
+
+def test_search_post_keyword(client):
+    query = {
+        'keyword': 'title0'
+    }
+    rv = client.get('/blog/user01', query_string=query, follow_redirects=True)
+    assert 'u01_body0' in rv.data.decode('utf-8')
+    assert 'u01_body9' not in rv.data.decode('utf-8')
+    assert 'u02' not in rv.data.decode('utf-8')
+
+
+def test_search_post_page(client):
+    query = {
+        'page': 2
+    }
+    rv = client.get('/blog/user01', query_string=query, follow_redirects=True)
+    assert 'title0' in rv.data.decode('utf-8')
+    assert 'title9' not in rv.data.decode('utf-8')
+    assert 'u02' not in rv.data.decode('utf-8')
+
+
+def test_edit_post(client):
+    login(client)
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'fas fa-trash-alt' in rv.data.decode('utf-8')
+    rv = client.get('/blog/user02', follow_redirects=True)
+    assert 'fas fa-trash-alt' not in rv.data.decode('utf-8')
+
+
+def test_bookmark_user_login(client):
+    login(client)
+    rv = client.get('/blog/user02', follow_redirects=True)
+    assert 'addBookmarkUser' in rv.data.decode('utf-8')
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'addBookmarkUser' not in rv.data.decode('utf-8')
+
+
+def test_bookmark_user_logout(client):
+    rv = client.get('/blog/user02', follow_redirects=True)
+    assert 'addBookmarkUser' not in rv.data.decode('utf-8')
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'addBookmarkUser' not in rv.data.decode('utf-8')
+
+
+def test_bookmark_post_login(client):
+    login(client)
+    rv = client.get('/blog/user02', follow_redirects=True)
+    assert 'addBookmarkPost' in rv.data.decode('utf-8')
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'addBookmarkPost' in rv.data.decode('utf-8')
+
+
+def test_bookmark_post_logout(client):
+    rv = client.get('/blog/user02', follow_redirects=True)
+    assert 'addBookmarkPost' not in rv.data.decode('utf-8')
+    rv = client.get('/blog/user01', follow_redirects=True)
+    assert 'addBookmarkPost' not in rv.data.decode('utf-8')
